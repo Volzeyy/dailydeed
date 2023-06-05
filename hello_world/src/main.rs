119dbe69839bd83f5905a25775de7fb2592d719b
Voldemar <vezedoes@gmail.com>
2023-06-05 21:25:03 +0300

    Day 1. A tiny bit of Rust

---
+++ hello_world/src/main.rs
@@ -0,0 +1,18 @@
+use rand::prelude::*;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let coin_flip: f64 = rng.gen();
+
+    if coin_flip < 0.5 {
+        std::println!("Tails");
+        return;
+    }
+
+    if coin_flip > 0.5 {
+        std::println!("Coin");
+        return;
+    };
+
+    std::println!("The coin blew up D:");
+}
